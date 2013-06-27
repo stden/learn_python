b017a37748ef8da2b56f7e1df32108a1532e7c80
Denis Stepulenok <super.denis@gmail.com>
2013-06-27 16:46:11 +0400

    Разбор XML строки

--- parse_print.py
+++ parse_print.py
@@ -5,3 +5,22 @@
 d = dict(a=2, b=3, complexField={2: 3, 9: 3})
 s = "%s" % d
 print s
+
+# Разбор XML строки
+xml = """<foo>
+   <bar>
+      <type foobar="1"/>
+      <type foobar="2"/>
+   </bar>
+</foo>"""
+
+import xml.etree.ElementTree as ET
+
+foo = ET.fromstring(xml)
+print foo.tag  # foo
+
+for bar in foo:
+    print bar.tag  # bar
+    for typeElement in bar:
+        print typeElement.tag  # type
+        print typeElement.attrib['foobar']  # 1 2
